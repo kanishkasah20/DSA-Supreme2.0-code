b9e66195b72d403fc33e066d3584e169ed5d5811
kanishkasah20 <114860158+kanishkasah20@users.noreply.github.com>
2023-09-03 00:32:03 +0530

    h

--- Conditionals && Loops/NumberSystemDeci_Binary.cpp
+++ Conditionals && Loops/NumberSystemDeci_Binary.cpp
@@ -20,16 +20,30 @@ int DecimalToBinaryMethod2(int n){
     int i=0;
     while(n>0){
         int bit= (n & 1);
-        binaryno = (bit * pow(10, i)) + binaryno;
+        binaryno = (bit * pow(10, i++)) + binaryno;
         n = n >> 1;
-        i++;
     }
     return binaryno;
 }
 
+int binaryToDecimal(int n){
+    int decimal=0;
+    int i=0;
+    while(n){
+        int bit=n%10;
+        decimal= decimal+(bit*pow(2,i++));
+        n/=10;
+    }
+    return decimal;
+}
+
 int main(){
-    int n;
-    cin>>n;
-    int binary = DecimalToBinaryMethod2(n);
-    cout<<binary+1<<endl; //+1 kuki ans glt aa raha h
+    // int n;
+    // cin>>n;
+    // int binary = DecimalToBinaryMethod2(n);
+    // cout<<binary+1<<endl; //+1 kuki ans glt aa raha h
+
+    int binaryno;
+    cin>>binaryno;
+    cout<<binaryToDecimal(binaryno)<<endl;
 }
