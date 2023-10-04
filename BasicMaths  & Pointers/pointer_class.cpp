001921ce2ae16ddea59f6562d476f3f0d5bdecfa
kanishkasah20 <114860158+kanishkasah20@users.noreply.github.com>
2023-10-05 04:50:59 +0530

    update

---
+++ BasicMaths  & Pointers/pointer_class.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main(){
+// int a=5;
+// int* ptr= &a;
+// cout<<sizeof(ptr)<<endl;
+
+//null ptr
+// int*ptr=0;
+
+int a=100;
+int* ptr= &a; 
+
+// a =a+1;
+// ptr =ptr+1;
+
+cout<<a<<endl;
+cout<<ptr<<endl;
+cout<<ptr+1<<endl;
+}
+
