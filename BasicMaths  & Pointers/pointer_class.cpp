037f59fdae3f947285291b9a1c457b3c39ac6361
kanishkasah20 <114860158+kanishkasah20@users.noreply.github.com>
2023-10-08 23:02:50 +0530

    h

--- BasicMaths  & Pointers/pointer_class.cpp
+++ BasicMaths  & Pointers/pointer_class.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+
+//   char ch[10] = "babbar";
+//   cout << &ch[0];
+//   char* cptr = &ch;
+
+//   char ch[10] = "a";
+//   char *cptr = ch;
+//   cout << cptr;
 
 
-int main(){
-    // int a=5;
+//   int arr[5] = {1,2,3,4,5};
+//   arr = arr + 1;
+
+
+
+
+//   int a = 5;
+//   int* p = &a;
+
+//   p = p + 1;
+//   cout << *p;
+//   int* ptr = 0;
+//   cout << *ptr << endl;
+
+
+//   int a = 5;
+//   cout << a << endl;
+//   cout << "address of a : "<< &a << endl;
+  
+
+//   //pointer creation
+//   int* ptr = &a;
+//   //acessing value stored at address stored in ptr
+//   cout << ptr << endl;
+//   cout << "Accessing: " << *ptr << endl;
+//   cout << &ptr << endl;
+
+
+
+int a=5;
     // int* ptr= &a;
     // cout<<sizeof(ptr)<<endl;
 
@@ -20,5 +57,8 @@ int main(){
     cout<<a<<endl;
     cout<<ptr<<endl;
     cout<<ptr+1<<endl;
+
+  return 0;
 }
+
 
