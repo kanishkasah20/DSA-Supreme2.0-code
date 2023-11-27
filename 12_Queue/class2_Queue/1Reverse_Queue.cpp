f9fa153ac467ce109b7365ab98f523fa5aac2f68
kanishkasah20 <114860158+kanishkasah20@users.noreply.github.com>
2023-11-27 15:12:28 +0530

    update

--- 12_Queue/class2_Queue/1Reverse_Queue.cpp
+++ 12_Queue/class2_Queue/1Reverse_Queue.cpp
@@ -39,6 +39,63 @@ void reserveQueue(queue<int> &q){
     q.push(element);
  }
 
+ void reverseFirstKelement(queue<int> &q, int k){
+    stack<int> s;
+    int n= q.size();
+
+    if(k>n || k==0){
+        return;
+    }
+    //push first k element into stack
+    for(int i=0; i<k ; i++){
+        int temp = q.front();
+        q.pop();
+        s.push(temp);
+    }
+
+    //push all k element into queue
+
+    while(!s.empty()){
+        int temp = s.top();
+        s.pop();
+        q.push(temp);
+    }
+
+    //pop and push first(n-k) elements into queue again
+    for(int i=0; i<(n-k); i++){
+        int temp = q.front();
+        q.pop();
+        q.push(temp);
+    }
+ }
+
+  void interLeaveQueue(queue<int> &first){
+    queue<int> second;
+
+    //push first half of first queue in second queue
+    int size= first.size();
+    for(int i=0;i<size/2; i++){
+        int temp = first.front();
+        first.pop();
+
+        second.push(temp);
+    }
+
+    //Merge both the halves
+    //into the original queue - named as first
+    for(int i=0;i<size/2;i++){
+        int temp = second.front();
+        second.pop();
+
+        first.push(temp);
+
+        temp= first.front();
+        first.pop();
+        first.push(temp);
+
+    }
+  }
+
 int main(){
     queue<int> q;
 
@@ -51,7 +108,9 @@ int main(){
 
 
     // reserveQueue(q);
-    reverse(q);
+    // reverse(q);
+    // reverseFirstKelement(q,4);
+    interLeaveQueue(q);
 
     cout<<"Printing Queue: "<<endl;
     while(!q.empty()){
